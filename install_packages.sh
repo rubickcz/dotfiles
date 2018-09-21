080ced682aebaf075f54136d5bea8f5027724cdc
Ondřej Kulatý <kulaty.o@gmail.com>
2018-09-22 00:15:32 +0200

    Refactor install packeges script

--- install_packages.sh
+++ install_packages.sh
@@ -11,10 +11,10 @@ source ./functions.sh
 # ------------------------------------------------------------------------------
 
 # packages to be installed via pacman
-PACKAGES="ack alsa-utils audacity base-devel bash bash-completion cups chromium ctags feh freetype2 dina-font dmenu dnsutils dunst evince gimp gnupg gsimplecal gtk-engines gvim htop i3 inkscape libnotify mc network-manager-applet networkmanager-openvpn ntfs-3g ntp numlockx nvm openssh pass polkit python-isort python-jedi python-netifaces python-psutil python-virtualenvwrapper rxvt-unicode tk ttf-font-awesome ttf-dejavu udevil unrar unzip vlc xdg-user-dirs xdotool xf86-input-libinput xorg xorg-xinit xorg-xinput xorg-xrandr xxkb wget zip"
+PACKAGES="ack alsa-utils base-devel bash bash-completion cups chromium ctags feh freetype2 dina-font dmenu dnsutils dunst gnupg gsimplecal gtk-engines gvim htop i3 libnotify mc network-manager-applet networkmanager-openvpn ntfs-3g ntp numlockx openssh pass polkit python-isort python-jedi python-netifaces python-psutil python-virtualenvwrapper rxvt-unicode tk ttf-font-awesome ttf-dejavu udevil unrar unzip xdg-user-dirs xdotool xf86-input-libinput xorg xorg-xinit xorg-xinput xorg-xrandr xxkb wget zip"
 
-# TODO: separate necessary dependencies and nice to have programs like vlc, gimp, etc
-# transmission-gtk audacity
+# common apps
+APP_PACKAGES="audacity evince gimp inkscape transmission-gtk vlc"
 
 # packages to be installed via aurget
 AUR_PACKAGES="i3pystatus betaflight-configurator fbxkb imwheel python-colour python-pyalsaaudio"
@@ -37,7 +37,7 @@ function has_pkg() {
 
 # Install official pakcages
 msg "Installing official packages..."
-sudo pacman -Sy --needed $PACKAGES
+sudo pacman -Sy --needed $PACKAGES $APP_PACKAGES
 
 # Install Aurget
 if [ `has_pkg aurget` -ne 0 ]; then
