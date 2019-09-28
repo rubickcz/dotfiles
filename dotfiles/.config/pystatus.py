dd674e42bf393b60159691450bfa0f890a8a2549
Ondřej Kulatý <kulaty.o@gmail.com>
2019-09-28 12:10:16 +0200

    Tune i3 status

--- dotfiles/.config/pystatus.py
+++ dotfiles/.config/pystatus.py
@@ -1,127 +1,148 @@
 import socket
 
 from i3pystatus import Status
-#from i3pystatus.updates import pacman
+from i3pystatus.updates import pacman
 from i3pystatus.weather import weathercom
 
 status = Status()
 
 # clock
 status.register(
-    "clock",
-    format="%a %b %-d %H:%M",
-    on_leftclick="gsimplecal"
+    'clock',
+    format='\uf073 %a %b %-d %H:%M',
+    on_leftclick='gsimplecal',
+)
 
+# keyboard layout
+status.register(
+    'xkblayout',
+    format='\uf11c {symbol}',
+    color='#6b7cff',
+    on_upscroll=None,
+    on_downscroll=None,
+)
+
+# redshift
+status.register(
+    'redshift',
+    format='\uf0eb {inhibit}',
+    color='#ff8c00',
+    format_inhibit=['ON', 'OFF'],
+    redshift_parameters=['-l', '50.08804:14.42076'],
 )
 
 # audio volume
 status.register(
-    "pulseaudio",
-    format="\u3000{volume}%",
-    color_muted="#FFFF00"
+    'pulseaudio',
+    format='\uf028 {volume}%',
+    color_muted='#FFFF00',
 )
 
-# brightness
-status.register(
-    "backlight",
-    format="\u3000{percentage}%",
-    backlight="intel_backlight",
-    on_upscroll=None,
-    on_downscroll=None,
-    interval=1
-)
+if socket.gethostname() != 'golem':
 
-# battery
-status.register(
-    'battery',
-    format='\u3000{percentage:.0f}%[ {consumption:.2f}W][ ({remaining})]',
-    not_present_text="No battery",
-    alert=True,
-    alert_percentage=5,
-    color="#ffc700",
-    interval=5,
-)
+    # brightness
+    status.register(
+        'backlight',
+        format='\uf185 {percentage}%',
+        backlight='intel_backlight',
+        on_upscroll=None,
+        on_downscroll=None,
+        interval=1,
+    )
 
-# audio pre-amp
-#status.register(
-#    "alsa",
-#    mixer="Pre-Amp",
-#    format="Pre: {volume}%",
-#    color_muted="#FFFF00",
-#    on_upscroll = ["increase_volume", 20],
-#    on_downscroll = ["decrease_volume", 20]
-#)
+    # battery
+    status.register(
+        'battery',
+        format='\uf240 {percentage:.0f}%[ {consumption:.2f}W][ ({remaining})]',
+        color='#ffc700',
+        not_present_text='No battery',
+        alert=True,
+        alert_percentage=5,
+        interval=5,
+    )
+
+if socket.gethostname() == 'golem':
+
+    # gpu temp
+    status.register(
+        'gpu_temp',
+        format='\uf1b2 {temp:.0f}°C',
+        color='#84daff',
+    )
 
 if socket.gethostname() in ['golem', 'guzzlord']:
-    # for golem machine (desktop pc), use thermal zone #2
     tz = '2'
 else:
-    # other machines use thermal zone #0
     tz = '0'
 
 # cpu temp
 status.register(
-    "temp",
-    format="{temp:.0f}°C",
-    color="#84daff",
-    file="/sys/class/thermal/thermal_zone"+tz+"/temp"
+    'temp',
+    format='\uf2c8 {temp:.0f}°C',
+    color='#84daff',
+    file='/sys/class/thermal/thermal_zone'+tz+'/temp',
 )
 
 # cpu frequency
 status.register(
-    "cpu_freq",
-    format="{avgg} Ghz",
-    color="#84daff"
+    'cpu_freq',
+    format='\uf3fd {avgg} GHz',
+    color='#84daff',
 )
 
 # load
 status.register(
-    "load",
-    format="\uf0e7\u3000{avg1}",
-    color="#84daff"
+    'load',
+    format='\uf496 {avg1}',
+    color='#84daff',
 )
 
 # available RAM
 status.register(
-    "mem",
-    format="\u3000{percent_used_mem}%",
-    divisor=1024**3
+    'mem',
+    format='\uf2db {percent_used_mem}%',
 )
 
 # free disk space
-status.register("disk",
-    path="/",
-    format="\u3000{avail}G"
+status.register('disk',
+    path='/',
+    format='\uf1c0 {avail} GB',
 )
 
+# external ip
+status.register(
+    'external_ip',
+    format='{ip}({country_code})',
+    color='#4682b4',
+    on_leftclick=None,
+    on_rightclick=None,
+)
 
 # ping
 status.register(
-    "ping",
-    format="\uF079\u3000{ping} ms",
-    format_down="unreachable",
-    color="#1ad892"
+    'ping',
+    format='\uf079 {ping} ms',
+    format_down='unreachable',
+    color='#1ad892',
 )
 
-# network usage
-#status.register(
-#   "network",
-#   format_up=" {bytes_recv:3d} MB/s  {bytes_sent} MB/s",
-#    format_up="{network_graph}",
-#    graph_style="braille-fill",
-#    graph_width=30,
-#    format_down="NET DOWN",
-#    interface="enp2s0",
-#)
-
 # pacman updates
-#status.register(
-#    "updates",
-#    format="  {count}",
-#    backends = pacman.Pacman(),
-#    color="#ffa99e"
-#)
+status.register(
+    'updates',
+    format='\uf019 {count}',
+    backends = pacman.Pacman(),
+    color='#ffa99e',
+)
 
+# bitcoin
+status.register(
+    'bitcoin',
+    format='\uf15a {last_price}',
+    color='#ffd700',
+    on_rightclick=None,
+    on_upscroll=None,
+    on_downscroll=None,
+)
 
 # weather
 status.register(
